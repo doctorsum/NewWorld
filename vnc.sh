9353b1736cba76338f9dbf1e6b3dc6196fa854f0
doctorsum <omar.thomas9999@gmail.com>
2024-08-21 21:09:45 +0300

    Delete vnc.sh

--- vnc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-
-wget 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz'
-sudo tar -xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
-
-ngrok authtoken 2jYcgOsDMlF7NsR3R5pc0BWBaNJ_WMJyYJah3Bs2QUNnGppu 
