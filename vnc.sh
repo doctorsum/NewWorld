af4a13f94b2d2eae600d6ea4b58a5d4fda931262
SudoSuII <premuim7281@gmail.com>
2024-08-21 17:55:51 +0300

    Create vnc.sh

---
+++ vnc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+wget 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz'
+sudo tar -xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+
+ngrok authtoken 2jYcgOsDMlF7NsR3R5pc0BWBaNJ_WMJyYJah3Bs2QUNnGppu 
+ 
+ngrok http --domain=dogfish-intimate-noticeably.ngrok-free.app 8083
