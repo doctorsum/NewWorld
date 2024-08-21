55c8e12476b8933946b1a22f77622475b97000cc
doctorsum <omar.thomas9999@gmail.com>
2024-08-21 20:54:44 +0300

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -4,5 +4,3 @@ wget 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz'
 sudo tar -xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 
 ngrok authtoken 2jYcgOsDMlF7NsR3R5pc0BWBaNJ_WMJyYJah3Bs2QUNnGppu 
-
-ngrok http --domain=dogfish-intimate-noticeably.ngrok-free.app 8083
