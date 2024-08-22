cd4705d17b85902e8543ceed383b4643bf054a21
doctorsum <omar.thomas9999@gmail.com>
2024-08-22 22:48:12 +0300

    Update start-vnc.sh

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# تشغيل Xvfb (إذا لم يكن لديك X Server)
 Xvfb :1 -screen 0 1920x720x24 &
 
-# تشغيل x11vnc
+# إعداد متغير DISPLAY
+export DISPLAY=:1
+
+startxfce4 &
+
 x11vnc -display :1 -forever -rfbauth /root/.vnc/passwd
